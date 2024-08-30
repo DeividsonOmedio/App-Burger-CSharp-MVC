81336066186a1ffa39aa27ae812d585c0221c939
DeividsoOmedio <deividsonomedio@outlook.com>
2024-08-30 18:26:19 -0300

    add identity

---
+++ Mvc/Areas/Identity/Data/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Areas.Identity.Data
+{
+    public class UserModel : IdentityUser
+    {
+        [MaxLength(50, ErrorMessage = "O tamanho máximo é de 50 caracteres")]
+        public string UserName { get; set; }
+        [MaxLength(15, ErrorMessage = "O tamanho máximo é de 15 caracteres")]
+        public string PhoneNumber { get; set; }
+    }
+}
